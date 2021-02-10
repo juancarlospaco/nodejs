e909ab087e2c1f3499eccf66099e78f6e37f5dc6
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-09 22:03:33 -0300

    Fix

--- src/nodejs/jsreadline.nim
+++ src/nodejs/jsreadline.nim
@@ -1,4 +1,4 @@
-## .. warning:: You MUST import `process` before importing this module.
+## .. warning:: You MUST import `process` before importing this module. Uses `stdout` by default.
 
 func importReadline*() {.importjs: "import * as readline from 'readline'@".}
   ## Alias for `import * as module_name from 'module_name';`. **Must be called once before using the module**
@@ -6,14 +6,14 @@ func importReadline*() {.importjs: "import * as readline from 'readline'@".}
 func requireReadline*() {.importjs: "const readline = require('readline')@".}
   ## Alias for `const module_name = require('module_name');`. **Must be called once before using the module**
 
-func clearLine*(s = "process.stout".cstring; dir = 0.cint): bool {.importjs: "readline.$1(#, #)".}
+func clearLine*(dir = 0.cint): bool {.importjs: "readline.$1(process.stout, #)".}
   ## https://nodejs.org/docs/latest/api/readline.html#readline_readline_clearline_stream_dir_callback
 
-func clearScreenDown*(s = "process.stout".cstring): bool {.importjs: "readline.$1(#)".}
+func clearScreenDown*(): bool {.importjs: "readline.$1(process.stout)".}
   ## https://nodejs.org/docs/latest/api/readline.html#readline_readline_clearscreendown_stream_callback
 
-func cursorTo*(s = "process.stout".cstring; x = 0.cint; y = 0.cint): bool {.importjs: "readline.$1(#, #, #)".}
+func cursorTo*(x, y: cint): bool {.importjs: "readline.$1(process.stout, #, #)".}
   ## https://nodejs.org/docs/latest/api/readline.html#readline_readline_cursorto_stream_x_y_callback
 
-func moveCursor*(s = "process.stout".cstring; dx = 0.cint; dy = 0.cint): bool {.importjs: "readline.$1(#, #, #)".}
+func moveCursor*(dx, dy: cint): bool {.importjs: "readline.$1(process.stout, #, #)".}
   ## https://nodejs.org/docs/latest/api/readline.html#readline_readline_movecursor_stream_dx_dy_callback
