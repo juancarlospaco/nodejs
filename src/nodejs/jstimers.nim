3313602216cd8854f6a7cd741eb9ca9f98dcad1f
Juan Carlos <JuanCarlospaco@gmail.com>
2021-03-01 19:35:43 -0300

    Add Timers module

--- src/nodejs/jstimers.nim
+++ src/nodejs/jstimers.nim
@@ -39,7 +39,8 @@ func setInterval*[T](callback: T; delay: cint): Timeout {.importjs: "timers.$1(#
 
 func setTimeoutPromise*(delay: cint; value: auto): Future[Timeout] {.importjs: "timersPromises.setTimeout(#, #)".}
   ## https://nodejs.org/api/timers.html#timers_timerspromises_settimeout_delay_value_options
-  ## .. note:: You MUST import `timersPromises` before importing this func.
+  ##
+  ## .. Note:: You MUST import `timersPromises` before importing this func.
 
 func setImmediatePromise*(value: auto): Future[Immediate] {.importjs: "timersPromises.setImmediate(#)".}
   ## https://nodejs.org/api/timers.html#timers_timerspromises_setimmediate_value_options
