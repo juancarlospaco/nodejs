2b75a008f31f818ca32c6356fcf22f73811c1f3e
Juan Carlos <JuanCarlospaco@gmail.com>
2021-03-01 02:59:52 -0300

    Add Readline

--- src/nodejs/jsreadline.nim
+++ src/nodejs/jsreadline.nim
@@ -1,4 +1,45 @@
 ## .. warning:: You MUST import `process` before importing this module. Uses `stdout` by default.
+import std/jsffi
+
+type
+  Readline* = ref object of JsRoot ## https://nodejs.org/api/readline.html#readline_readline
+
+let
+  line* {.importjs: "readline.line".}: cstring  ## https://nodejs.org/api/readline.html#readline_rl_line
+  cursor* {.importjs: "readline.cursor".}: cint ## https://nodejs.org/api/readline.html#readline_rl_cursor
+
+func newReadline*(): Readline {.importjs: "readline.createInterface({input: process.stdin, output: process.stdout})".}
+  ## https://nodejs.org/api/readline.html#readline_readline
+
+func close*(self: Readline) {.importjs: "#.$1()".}
+  ## https://nodejs.org/api/readline.html#readline_rl_close
+
+func pause*(self: Readline) {.importjs: "#.$1()".}
+  ## https://nodejs.org/api/readline.html#readline_rl_pause
+
+func prompt*(self: Readline; preserveCursor: bool) {.importjs: "#.$1(#)".}
+  ## https://nodejs.org/api/readline.html#readline_rl_prompt_preservecursor
+
+func question*[T](self: Readline; query: cstring; callback: T) {.importjs: "#.$1(#, #)".}
+  ## https://nodejs.org/api/readline.html#readline_rl_question_query_options_callback
+
+func resume*(self: Readline) {.importjs: "#.$1()".}
+  ## https://nodejs.org/api/readline.html#readline_rl_resume
+
+func setPrompt*(self: Readline; prompt: cstring) {.importjs: "#.$1(#)".}
+  ## https://nodejs.org/api/readline.html#readline_rl_setprompt_prompt
+
+func getPrompt*(self: Readline): cstring {.importjs: "#.$1()".}
+  ## https://nodejs.org/api/readline.html#readline_rl_getprompt
+
+func write*(self: Readline; data: cstring) {.importjs: "#.$1(#)".}
+  ## https://nodejs.org/api/readline.html#readline_rl_write_data_key
+
+func write*(self: Readline; data: cstring; ctrl: bool; meta: bool; shift: bool; name: cstring) {.importjs: "#.$1(#, {ctrl: #, meta: #, shift: #, name: #})".}
+  ## https://nodejs.org/api/readline.html#readline_rl_write_data_key
+
+func getCursorPos*(self: Readline): JsObject {.importjs: "#.$1()".}
+  ## https://nodejs.org/api/readline.html#readline_rl_getcursorpos
 
 func importReadline*() {.importjs: "import * as readline from 'readline'@".}
   ## Alias for `import * as module_name from 'module_name';`. **Must be called once before using the module**
@@ -20,10 +61,21 @@ func moveCursor*(dx, dy: cint): bool {.importjs: "readline.$1(process.stout, #, 
 
 
 runnableExamples:
-  import nodejs/jsprocess
+  import std/jsffi, nodejs/jsprocess
   requireProcess()
   requireReadline()
-  doAssert clearLine()
-  doAssert clearScreenDown()
-  doAssert cursorTo(x = 0.cint, y = 0.cint)
-  doAssert moveCursor(dx = 0.cint, dy = 0.cint)
+  block:
+    doAssert clearLine()
+    doAssert clearScreenDown()
+    doAssert cursorTo(x = 0.cint, y = 0.cint)
+    doAssert moveCursor(dx = 0.cint, dy = 0.cint)
+  block:
+    let rl: Readline = newReadline()
+    rl.setPrompt(">>>".cstring)
+    doAssert rl.getPrompt() == ">>>".cstring
+    doAssert rl.getCursorPos() is JsObject
+    rl.pause()
+    rl.resume()
+    rl.write("data".cstring)
+    rl.close()
+    echo cursor
