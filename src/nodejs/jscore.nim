335c48be691e2275d30118f28e267ad9ee7a59a5
Juan Carlos <JuanCarlospaco@gmail.com>
2021-03-11 11:57:45 -0300

    update the updates

--- src/nodejs/jscore.nim
+++ src/nodejs/jscore.nim
@@ -102,6 +102,15 @@ func toExponential*(n: SomeFloat; fractionDigits: Positive): cstring {.importjs:
 
 func toPrecision*(n: SomeFloat; fractionDigits: Positive): cstring {.importjs: "#.$1(#)".}
 
+func `&&=`*(lhs, rhs: auto): auto {.importjs: "(# &&= #)", discardable.}
+  ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND_assignment
+
+func `||=`*(lhs, rhs: auto): auto {.importjs: "(# ||= #)", discardable.}
+  ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR_assignment
+
+func `??=`*(lhs, rhs: auto): auto {.importjs: "(# ??= #)", discardable.}
+  ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_nullish_assignment
+
 func indentation*(s: cstring): cint {.importjs: """
   (() => {
     const m = #.match(/^[\s\\t]*/gm);
