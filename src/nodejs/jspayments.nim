2c02a6be630ec8e66bf9784cc2f263ff537243c1
Juan Carlos <juancarlospaco@gmail.com>
2021-08-08 16:15:23 -0300

    Add Payments API

---
+++ src/nodejs/jspayments.nim
@@ -0,0 +1,44 @@
+## Payments API.
+## * https://github.com/w3c/payment-request-info/wiki/FAQ
+##
+## .. warning:: Experimental browser API, we hope browsers support it someday, untested because it wont work at the time of writing it, and is not widely used yet.
+when not defined(js):
+  {.fatal: "Module jspayments is designed to be used with the JavaScript backend.".}
+
+import std/[jsffi, asyncjs]
+
+type
+  PaymentRequest* = ref object of JsRoot ## https://developer.mozilla.org/en-US/docs/Web/API/Payment_Request_API
+    shippingAddress*: JsObject ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentRequest/shippingAddress
+    shippingOption*: JsObject  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentRequest/shippingOption
+
+  PaymentResponse* = ref object of JsRoot  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse
+    details*: JsObject         ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/details
+    methodName*: cstring       ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/methodName
+    payerEmail*: cstring       ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/payerEmail
+    payerName*: cstring       ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/payerName
+    payerPhone*: cstring       ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/payerPhone
+    requestId*: cstring        ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/requestId
+    shippingAddress*: JsObject ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/shippingAddress
+    shippingOption*: JsObject  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentRequest/shippingOption
+
+func newPaymentRequest*(methodData, details, options: JsObject): PaymentRequest {.importjs: "new PaymentRequest(#, #, #)".}
+  ## https://developer.mozilla.org/en-US/docs/Web/API/Payment_Request_API
+
+func newPaymentRequest*(methodData, details: JsObject): PaymentRequest {.importjs: "new PaymentRequest(#, #)".}
+  ## https://developer.mozilla.org/en-US/docs/Web/API/Payment_Request_API
+
+func canMakePayment*(self: PaymentRequest): Future[bool] {.importjs: "#.$1()".}
+  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentRequest/canMakePayment
+
+func show*(self: PaymentRequest): Future[PaymentResponse] {.importjs: "#.$1()".}
+  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentRequest/show
+
+func retry*(self: PaymentRequest | PaymentResponse): Future[PaymentResponse] {.importjs: "#.$1()".}
+  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/retry
+
+func complete*(self: PaymentRequest | PaymentResponse): Future[void] {.importjs: "#.$1()".}
+  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/complete
+
+func complete*(self: PaymentRequest | PaymentResponse; results: cstring): Future[void] {.importjs: "#.$1(#)".}
+  ## https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse/complete
