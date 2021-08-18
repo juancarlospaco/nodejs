1515e821d2c4022cfaae318d7035f768d27843c6
Juan Carlos <juancarlospaco@gmail.com>
2021-08-18 19:01:17 -0300

    Update doc

--- src/nodejs/jspayments.nim
+++ src/nodejs/jspayments.nim
@@ -1,7 +1,7 @@
 ## Payments API.
 ## * https://github.com/w3c/payment-request-info/wiki/FAQ
 ##
-## .. warning:: Experimental JavaScript API, we hope browsers support it someday, untested because it wont work at the time of writing it, and is not widely used yet.
+## .. warning:: Experimental JavaScript API, we hope browsers support it soon, untested because is not widely used at the time of writing it.
 when not defined(js):
   {.fatal: "Module jspayments is designed to be used with the JavaScript backend.".}
 
