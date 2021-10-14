69df69aee27de09648bd748c445400f44618b56f
Juan Carlos <juancarlospaco@gmail.com>
2021-10-14 06:19:59 -0700

    update

--- src/nodejs/jsdebugutils.nim
+++ src/nodejs/jsdebugutils.nim
@@ -2,6 +2,8 @@ import std/macros
 
 macro debugs*(frequency: static[int]; debugInBetween, codeToDebug: untyped) =
   ## Debug helper, injects `debugInBetween` in-between `codeToDebug` with given `frequency`, set `frequency` to `0` to disable debug.
+  ##
+  ## .. Warning:: Designed for debugging purposes only, remove after debugging.
   doAssert frequency >= 0, "frequency must be a positive integer >= 0"
   if frequency == 0: return codeToDebug  # If frequency is 0 do nothing.
   result = newTree(nnkStmtList)
@@ -10,13 +12,13 @@ macro debugs*(frequency: static[int]; debugInBetween, codeToDebug: untyped) =
     result.add line
     inc freqCounter
     if freqCounter mod frequency == 0:
-      let infos = $debugCounter & '\t' & lineInfo(line) & '\t'
-      result.add(quote do: echo(`infos`, getOccupiedMem()))
+      let s = $debugCounter & '\t' & lineInfo(line)
+      result.add nnkStmtList.newTree(nnkCall.newTree(newIdentNode"echo", newLit(s)))
       inc debugCounter
       result.add debugInBetween
 
 runnableExamples:
-  debugs 2, echo("INBETWEEN"):
+  debugs 2, echo("INBETWEEN ", i > 0):
     var i = 1
     let x = 9
     var s = "a"
@@ -31,16 +33,16 @@ runnableExamples:
 ## .. code-block:: nim
 ##    var i = 1
 ##    let x = 9
-##    echo("INBETWEEN")
+##    echo("INBETWEEN ", i > 0)
 ##    var s = "a"
 ##    i = 42
-##    echo("INBETWEEN")
+##    echo("INBETWEEN ", i > 0)
 ##    i = i + x
 ##    i = x
-##    echo("INBETWEEN")
+##    echo("INBETWEEN ", i > 0)
 ##    s.add 'x'
 ##    i = x + x
-##    echo("INBETWEEN")
+##    echo("INBETWEEN ", i > 0)
 ##
 ## - `debugInBetween` can be any "debugging code", like `echo`, `assert`, checks, dumps, etc.
 ## - `debugInBetween` is meant to be like `echo foo`, `assert foo != nil`, `echo foo.repr`, etc.
