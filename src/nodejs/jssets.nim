8dd576ed96c86494a276e2ef69b85a923a50d7d6
Juan Carlos <juancarlospaco@gmail.com>
2021-12-21 17:53:13 -0800

    Some tiny fixes

--- src/nodejs/jssets.nim
+++ src/nodejs/jssets.nim
@@ -24,7 +24,7 @@ func clear*(this: JsSet) {.importjs: "#.$1()".}
 func contains*(this: JsSet; value: JsObject): bool {.importjs: "#.has(#)".}
   ## https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/has
 
-func toString*(this: JsSet): seq[cstring] {.importjs: """
+func toCstring*(this: JsSet): seq[cstring] {.importjs: """
   (() => {const result = []; #.forEach(item => result.push(JSON.stringify(item))); return result})()""".}
   ## Convert `JsSet` to `seq[cstring]`, all items will be converted to `cstring`.
 
@@ -35,20 +35,19 @@ func `$`*(this: JsSet): string = $this.toString()
 
 runnableExamples:
   import std/jsffi
-  if defined(fusionJsSetTests):
-    let a: JsSet = newJsSet([1.toJs, 2.toJs, 3.toJs, 4.toJs])
-    let b: JsSet = newJsSet([1.0.toJs, 2.0.toJs, 3.0.toJs])
-    doAssert a.len == 4
-    doAssert b.len == 3
-    doAssert a.toString() == @["1".cstring, "2", "3", "4"]
-    doAssert b.toString() == @["1".cstring, "2", "3"]
-    a.clear()
-    b.clear()
-    let d: JsSet = newJsSet([1.toJs, 2.toJs, 3.toJs])
-    doAssert d.len == 3
-    d.add(4.toJs)
-    d.delete(2.toJs)
-    doAssert 3.toJs in d
-    doAssert "3".cstring.toJs notin d
-    doAssert d.contains 1.toJs
-    doAssert $d == """@["1", "3", "4"]"""
+  let a: JsSet = newJsSet([1.toJs, 2.toJs, 3.toJs, 4.toJs])
+  let b: JsSet = newJsSet([1.0.toJs, 2.0.toJs, 3.0.toJs])
+  doAssert a.len == 4
+  doAssert b.len == 3
+  doAssert a.toCstring() == @["1".cstring, "2", "3", "4"]
+  doAssert b.toCstring() == @["1".cstring, "2", "3"]
+  a.clear()
+  b.clear()
+  let d: JsSet = newJsSet([1.toJs, 2.toJs, 3.toJs])
+  doAssert d.len == 3
+  d.add(4.toJs)
+  d.delete(2.toJs)
+  doAssert 3.toJs in d
+  doAssert "3".cstring.toJs notin d
+  doAssert d.contains 1.toJs
+  doAssert $d == """@["1", "3", "4"]"""
