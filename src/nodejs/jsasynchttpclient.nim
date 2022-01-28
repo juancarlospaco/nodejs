b2e888aae5749b616b9a875ffef90afc9e1d72e7
Juan Carlos <juancarlospaco@gmail.com>
2022-01-28 15:39:58 -0800

    fixes

--- src/nodejs/jsasynchttpclient.nim
+++ src/nodejs/jsasynchttpclient.nim
@@ -8,7 +8,7 @@ type JsAsyncHttpClient* = ref object of JsRoot
 
 func newJsAsyncHttpClient*(): JsAsyncHttpClient = discard
 
-template fetchOptionsImpl(body; metod: static[cstring]): FetchOptions =
+template fetchOptionsImpl(body; metod): FetchOptions =
   unsafeNewFetchOptions(metod = metod, body = body, mode = "cors".cstring, credentials = "include".cstring,
     cache = "default".cstring, referrerPolicy = "unsafe-url".cstring, keepalive = false)
 
