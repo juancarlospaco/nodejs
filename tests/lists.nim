395e5e6fcd26cb7a35c09c27d3c8e63a3684d165
Juan Carlos <juancarlospaco@gmail.com>
2022-06-27 19:37:48 -0300

    Make internal fork open source

---
+++ tests/lists.nim
@@ -0,0 +1,19 @@
+import strutils
+import karax/karax
+
+var contents: seq[cstring] = @[]
+
+proc onTodoEnter(e: Event; n: VNode) =
+  contents.insert(n.value & "BBBB")
+  contents.insert(n.value)
+  n.value = ""
+
+proc createDom(): VNode =
+  result = buildHtml(tdiv):
+    input(class = "new-todo", placeholder="What needs to be done?", name = "newTodo",
+          onkeyupenter = onTodoEnter, setFocus)
+    for c in contents:
+      tdiv:
+        text c
+
+setRenderer createDom
