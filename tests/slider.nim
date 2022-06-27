6bd4ab6ef6d7dfb619fdd733c7d1047ec33888fe
Juan Carlos <juancarlospaco@gmail.com>
2022-06-27 20:25:52 -0300

    Make internal fork open source

---
+++ tests/slider.nim
@@ -0,0 +1,17 @@
+import nodejs/karax
+
+let id = "foo1"
+let id2 = "foo2"
+let valueInitial = "150"
+var value = valueInitial
+proc updateText() =
+  value = $document.getElementById($id).value
+proc createDom(): VNode =
+  result = buildHtml(tdiv):
+    input(`type`="range", min = "10", max = "170", value = valueInitial, id = id):
+      proc oninput(ev: Event; target: VNode) = updateText()
+    tdiv(id=id2):
+      text value
+
+
+setRenderer createDom, "ROOT"
