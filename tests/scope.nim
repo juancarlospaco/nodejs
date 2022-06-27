e6bd689592f772a5113f8fa76b266e4d55ee7d18
Juan Carlos <juancarlospaco@gmail.com>
2022-06-27 19:37:57 -0300

    Make internal fork open source

---
+++ tests/scope.nim
@@ -0,0 +1,14 @@
+import karax/karax
+
+proc render():VNode =
+  build_html tdiv:
+    block:
+      let a = 1
+      tdiv():
+        text $a
+    block:
+      let a = 2
+      tdiv():
+        text $a
+
+setRenderer render
