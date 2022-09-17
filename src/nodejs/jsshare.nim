f2c72f93f3940196574a992115c5e4dbf7777cdf
Juan Carlos <juancarlospaco@gmail.com>
2022-09-17 19:19:35 -0300

    Improve type-safety for return types

--- src/nodejs/jsshare.nim
+++ src/nodejs/jsshare.nim
@@ -4,16 +4,16 @@
 ##
 ## .. image:: https://raw.githubusercontent.com/juancarlospaco/nodejs/main/docs/nodejs/jsshare.jpg
 
-func canShare*(): bool {.importjs: "Navigator.$1()".}
+func canShare*(): bool {.importjs: "(Navigator.$1() || false)".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Navigator/canShare
 
-func canShare*(url: cstring): bool {.importjs: "Navigator.$1({url: #})".}
+func canShare*(url: cstring): bool {.importjs: "(Navigator.$1({url: #}) || false)".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Navigator/canShare
 
-func canShare*(text, url: cstring): bool {.importjs: "Navigator.$1({text: #, url: #})".}
+func canShare*(text, url: cstring): bool {.importjs: "(Navigator.$1({text: #, url: #}) || false)".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Navigator/canShare
 
-func canShare*(title, text, url: cstring): bool {.importjs: "Navigator.$1({title: #, text: #, url: #})".}
+func canShare*(title, text, url: cstring): bool {.importjs: "(Navigator.$1({title: #, text: #, url: #}) || false)".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Navigator/canShare
 
 func share*(url: cstring) {.importjs: "(await Navigator.$1({url: #}) || undefined)", discardable.}
