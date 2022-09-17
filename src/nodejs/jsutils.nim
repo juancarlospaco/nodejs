ca7e5991ff6d705687697fde37964ddaf63bf715
Juan Carlos <juancarlospaco@gmail.com>
2022-09-17 19:42:35 -0300

    Improve type-safety for return types

--- src/nodejs/jsutils.nim
+++ src/nodejs/jsutils.nim
@@ -42,10 +42,10 @@ func deprecate*[T](function: T; msg: cstring): T {.importjs: "util.$1(#, #)".}
 func deprecate*[T](function: T; msg, code: cstring): T {.importjs: "util.$1(#, #, #)".}
   ## https://nodejs.org/api/util.html#util_util_deprecate_fn_msg_code
 
-func format*(format: cstring; args: cstring): cstring {.importjs: "util.$1(#, @)", varargs.}
+func format*(format: cstring; args: cstring): cstring {.importjs: "(util.$1(#, @) || '')", varargs.}
   ## https://nodejs.org/api/util.html#util_util_format_format_args
 
-func getSystemErrorName*(err: cint): cstring {.importjs: "util.$1(#)".}
+func getSystemErrorName*(err: cint): cstring {.importjs: "(util.$1(#) || '')".}
   ## https://nodejs.org/api/util.html#util_util_getsystemerrorname_err
 
 func inherits*[T](constructor, superConstructor: T) {.importjs: "util.$1(#, #)".}
@@ -54,145 +54,145 @@ func inherits*[T](constructor, superConstructor: T) {.importjs: "util.$1(#, #)".
 func inspect*(objec: auto; showHidden = false; depth = 2.cint; colors = false) {.importjs: "util.$1(#, #, #, #)".}
   ## https://nodejs.org/api/util.html#util_util_inspect_object_showhidden_depth_colors
 
-func isDeepStrictEqual*(val1, val2: auto): bool {.importjs: "util.$1(#, #)".}
+func isDeepStrictEqual*(val1, val2: auto): bool {.importjs: "(util.$1(#, #) || false)".}
   ## https://nodejs.org/api/util.html#util_util_isdeepstrictequal_val1_val2
 
-func isAnyArrayBuffer*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isAnyArrayBuffer*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isanyarraybuffer_value
 
-func isArrayBufferView*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isArrayBufferView*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isarraybufferview_value
 
-func isArgumentsObject*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isArgumentsObject*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isargumentsobject_value
 
-func isArrayBuffer*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isArrayBuffer*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isarraybuffer_value
 
-func isAsyncFunction*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isAsyncFunction*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isasyncfunction_value
 
-func isBigInt64Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isBigInt64Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isbigint64array_value
 
-func isBigUint64Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isBigUint64Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isbiguint64array_value
 
-func isBooleanObject*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isBooleanObject*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isbooleanobject_value
 
-func isBoxedPrimitive*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isBoxedPrimitive*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isboxedprimitive_value
 
-func isDataView*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isDataView*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isdataview_value
 
-func isDate*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isDate*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isdate_value
 
-func isExternal*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isExternal*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isexternal_value
 
-func isFloat32Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isFloat32Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isfloat32array_value
 
-func isFloat64Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isFloat64Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isfloat64array_value
 
-func isGeneratorFunction*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isGeneratorFunction*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isgeneratorfunction_value
 
-func isGeneratorObject*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isGeneratorObject*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isgeneratorobject_value
 
-func isInt8Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isInt8Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isint8array_value
 
-func isInt16Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isInt16Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isint16array_value
 
-func isInt32Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isInt32Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isint32array_value
 
-func isMap*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isMap*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_ismap_value
 
-func isMapIterator*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isMapIterator*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_ismapiterator_value
 
-func isModuleNamespaceObject*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isModuleNamespaceObject*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_ismodulenamespaceobject_value
 
-func isNativeError*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isNativeError*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isnativeerror_value
 
-func isNumberObject*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isNumberObject*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isnumberobject_value
 
-func isPromise*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isPromise*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_ispromise_value
 
-func isProxy*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isProxy*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isproxy_value
 
-func isRegExp*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isRegExp*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isregexp_value
 
-func isSet*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isSet*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isset_value
 
-func isSetIterator*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isSetIterator*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_issetiterator_value
 
-func isSharedArrayBuffer*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isSharedArrayBuffer*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_issharedarraybuffer_value
 
-func isStringObject*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isStringObject*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isstringobject_value
 
-func isSymbolObject*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isSymbolObject*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_issymbolobject_value
 
-func isTypedArray*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isTypedArray*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_istypedarray_value
 
-func isUint8Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isUint8Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isuint8array_value
 
-func isUint8ClampedArray*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isUint8ClampedArray*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isuint8clampedarray_value
 
-func isUint16Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isUint16Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isuint16array_value
 
-func isUint32Array*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isUint32Array*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isuint32array_value
 
-func isWeakMap*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isWeakMap*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isweakmap_value
 
-func isWeakSet*(value: auto): bool {.importjs: "util.types.$1(#)".}
+func isWeakSet*(value: auto): bool {.importjs: "(util.types.$1(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_isweakset_value
 
-func isWebAssemblyCompiledModule*(value: auto): bool {.importjs: "(# instanceof WebAssembly.Module)".}
+func isWebAssemblyCompiledModule*(value: auto): bool {.importjs: "((# instanceof WebAssembly.Module) || false)".}
   ## https://nodejs.org/api/util.html#util_util_types_iswebassemblycompiledmodule_value
 
-func isBoolean*(value: auto): bool {.importjs: "(typeof # === 'boolean')".}
+func isBoolean*(value: auto): bool {.importjs: "((typeof # === 'boolean') || false)".}
   ## https://nodejs.org/api/util.html#util_util_isboolean_object
 
-func isBuffer*(value: auto): bool {.importjs: "Buffer.isBuffer(#)".}
+func isBuffer*(value: auto): bool {.importjs: "(Buffer.isBuffer(#) || false)".}
   ## https://nodejs.org/api/util.html#util_util_isbuffer_object
 
-func isFunction*(value: auto): bool {.importjs: "(typeof # === 'function')".}
+func isFunction*(value: auto): bool {.importjs: "((typeof # === 'function') || false)".}
   ## https://nodejs.org/api/util.html#util_util_isfunction_object
 
-func isNumber*(value: auto): bool {.importjs: "(typeof # === 'number')".}
+func isNumber*(value: auto): bool {.importjs: "((typeof # === 'number') || false)".}
   ## https://nodejs.org/api/util.html#util_util_isnumber_object
 
-func isString*(value: auto): bool {.importjs: "(typeof # === 'string')".}
+func isString*(value: auto): bool {.importjs: "((typeof # === 'string') || false)".}
   ## https://nodejs.org/api/util.html#util_util_isstring_object
 
-func isSymbol*(value: auto): bool {.importjs: "(typeof # === 'symbol')".}
+func isSymbol*(value: auto): bool {.importjs: "((typeof # === 'symbol') || false)".}
   ## https://nodejs.org/api/util.html#util_util_issymbol_object
 
 
