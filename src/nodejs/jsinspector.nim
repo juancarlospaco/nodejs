d255d62f0762643a9085a113f36a2cbdea59ae68
Juan Carlos <juancarlospaco@gmail.com>
2022-09-17 18:36:11 -0300

    Improve type-safety for return types

--- src/nodejs/jsinspector.nim
+++ src/nodejs/jsinspector.nim
@@ -26,60 +26,60 @@ func requireInspector*() {.importjs: "const inspector = require('inspector')@".}
 func close*(): cstring {.importjs: "inspector.$1()".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_close
 
-func inspectorConsoleLog*(args: auto): cstring {.importjs: "inspector.console.log(@)", varargs.}
+func inspectorConsoleLog*(args: auto): cstring {.importjs: "(inspector.console.log(@) || '')", varargs.}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleInfo*(args: auto): cstring {.importjs: "inspector.console.info(@)", varargs.}
+func inspectorConsoleInfo*(args: auto): cstring {.importjs: "(inspector.console.info(@) || '')", varargs.}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleWarn*(args: auto): cstring {.importjs: "inspector.console.warn(@)", varargs.}
+func inspectorConsoleWarn*(args: auto): cstring {.importjs: "(inspector.console.warn(@) || '')", varargs.}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleDebug*(args: auto): cstring {.importjs: "inspector.console.debug(@)", varargs.}
+func inspectorConsoleDebug*(args: auto): cstring {.importjs: "(inspector.console.debug(@) || '')", varargs.}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleError*(args: auto): cstring {.importjs: "inspector.console.error(@)", varargs.}
+func inspectorConsoleError*(args: auto): cstring {.importjs: "(inspector.console.error(@) || '')", varargs.}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleTrace*(args: auto): cstring {.importjs: "inspector.console.trace(@)", varargs.}
+func inspectorConsoleTrace*(args: auto): cstring {.importjs: "(inspector.console.trace(@) || '')", varargs.}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleClear*(): cstring {.importjs: "inspector.console.clear()".}
+func inspectorConsoleClear*(): cstring {.importjs: "(inspector.console.clear() || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleCount*(label = "".cstring): cstring {.importjs: "inspector.console.count(#)".}
+func inspectorConsoleCount*(label = "".cstring): cstring {.importjs: "(inspector.console.count(#) || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleCountReset*(label = "".cstring): cstring {.importjs: "inspector.console.countReset(#)".}
+func inspectorConsoleCountReset*(label = "".cstring): cstring {.importjs: "(inspector.console.countReset(#) ||'')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleGroup*(label = "".cstring): cstring {.importjs: "inspector.console.group(#)".}
+func inspectorConsoleGroup*(label = "".cstring): cstring {.importjs: "(inspector.console.group(#) || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleGroupCollapsed*(label = "".cstring): cstring {.importjs: "inspector.console.groupCollapsed(#)".}
+func inspectorConsoleGroupCollapsed*(label = "".cstring): cstring {.importjs: "(inspector.console.groupCollapsed(#) || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleGroupEnd*(): cstring {.importjs: "inspector.console.groupEnd()".}
+func inspectorConsoleGroupEnd*(): cstring {.importjs: "(inspector.console.groupEnd() || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func inspectorConsoleTable*(): cstring {.importjs: "inspector.console.table()".}
+func inspectorConsoleTable*(): cstring {.importjs: "(inspector.console.table() || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_console
 
-func open*(port: cint; host: cstring; wait = false): cstring {.importjs: "inspector.$1(#, #, #)".}
+func open*(port: cint; host: cstring; wait = false): cstring {.importjs: "(inspector.$1(#, #, #) || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_open_port_host_wait
 
-func open*(port: cint): cstring {.importjs: "inspector.$1(#)".}
+func open*(port: cint): cstring {.importjs: "(inspector.$1(#) || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_open_port_host_wait
 
-func open*(): cstring {.importjs: "inspector.$1()".}
+func open*(): cstring {.importjs: "(inspector.$1() || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_open_port_host_wait
 
-func url*(): cstring {.importjs: "inspector.$1()".}
+func url*(): cstring {.importjs: "(inspector.$1() || '')".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_url
 
 func waitForDebugger*() {.importjs: "inspector.$1()".}
   ## https://nodejs.org/api/inspector.html#inspector_inspector_waitfordebugger
 
-func toCstring*(self: InspectorSession): cstring {.importjs: "JSON.stringify(#)".}
+func toCstring*(self: InspectorSession): cstring {.importjs: "(JSON.stringify(#) || '')".}
 
 func `$`*(self: InspectorSession): string = $toCstring(self)
