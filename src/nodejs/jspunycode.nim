514740e0cc1514c6f9a2240edf277764a011d736
Juan Carlos <juancarlospaco@gmail.com>
2022-09-17 19:06:19 -0300

    Improve type-safety for return types

--- src/nodejs/jspunycode.nim
+++ src/nodejs/jspunycode.nim
@@ -5,22 +5,22 @@ func importPunycode*() {.importjs: "import * as punycode from 'punycode'@".}
 func requirePunycode*() {.importjs: "const punycode = require('punycode')@".}
   ## Alias for `const module_name = require('module_name');`. **Must be called once before using the module**
 
-func decode*(stringy: cstring): cstring {.importjs: "punycode.$1(#)".}
+func decode*(stringy: cstring): cstring {.importjs: "(punycode.$1(#) || '')".}
   ## https://nodejs.org/api/punycode.html#punycode_punycode_decode_string
 
-func encode*(stringy: cstring): cstring {.importjs: "punycode.$1(#)".}
+func encode*(stringy: cstring): cstring {.importjs: "(punycode.$1(#) || '')".}
   ## https://nodejs.org/api/punycode.html#punycode_punycode_encode_string
 
-func toASCII*(stringy: cstring): cstring {.importjs: "punycode.$1(#)".}
+func toASCII*(stringy: cstring): cstring {.importjs: "(punycode.$1(#) || '')".}
   ## https://nodejs.org/api/punycode.html#punycode_punycode_toascii_domain
 
-func toUnicode*(stringy: cstring): cstring {.importjs: "punycode.$1(#)".}
+func toUnicode*(stringy: cstring): cstring {.importjs: "(punycode.$1(#) || '')".}
   ## https://nodejs.org/api/punycode.html#punycode_punycode_tounicode_domain
 
-func ucs2Decode*(stringy: cstring): seq[cint] {.importjs: "punycode.ucs2.decode(#)".}
+func ucs2Decode*(stringy: cstring): seq[cint] {.importjs: "(punycode.ucs2.decode(#) || [])".}
   ## https://nodejs.org/api/punycode.html#punycode_punycode_ucs2_decode_string
 
-func ucs2Encode*(codePoints: openArray[cint]): cstring {.importjs: "punycode.ucs2.encode(#)".}
+func ucs2Encode*(codePoints: openArray[cint]): cstring {.importjs: "(punycode.ucs2.encode(#) || '')".}
   ## https://nodejs.org/api/punycode.html#punycode_punycode_ucs2_encode_codepoints
 
 
