660b9f60ff775aeaec4b0d3348bb883e520658d4
Juan Carlos <juancarlospaco@gmail.com>
2022-09-17 19:06:06 -0300

    Improve type-safety for return types

--- src/nodejs/jspath.nim
+++ src/nodejs/jspath.nim
@@ -8,40 +8,40 @@ func importPath*() {.importjs: "import * as path from 'path'@".}
 func requirePath*() {.importjs: "const path = require('path')@".}
   ## Alias for `const module_name = require('module_name');`. **Must be called once before using the module**
 
-func basename*(path: cstring): cstring {.importjs: "path.$1(#)".}
+func basename*(path: cstring): cstring {.importjs: "(path.$1(#) || '')".}
   ## https://nodejs.org/api/path.html#path_path_basename_path_ext
 
-func basename*(path, ext: cstring): cstring {.importjs: "path.$1(#, #)".}
+func basename*(path, ext: cstring): cstring {.importjs: "(path.$1(#, #) || '')".}
   ## https://nodejs.org/api/path.html#path_path_basename_path_ext
 
-func dirname*(path: cstring): cstring {.importjs: "path.$1(#)".}
+func dirname*(path: cstring): cstring {.importjs: "(path.$1(#) || '')".}
   ## https://nodejs.org/api/path.html#path_path_dirname_path
 
-func extname*(path: cstring): cstring {.importjs: "path.$1(#)".}
+func extname*(path: cstring): cstring {.importjs: "(path.$1(#) || '')".}
   ## https://nodejs.org/api/path.html#path_path_extname_path
 
-func isAbsolute*(path: cstring): bool {.importjs: "path.$1(#)".}
+func isAbsolute*(path: cstring): bool {.importjs: "(path.$1(#) || false)".}
   ## https://nodejs.org/api/path.html#path_path_isabsolute_path
 
-func join*(paths: cstring): cstring {.importjs: "path.$1(@)", varargs.}
+func join*(paths: cstring): cstring {.importjs: "(path.$1(@) || '')", varargs.}
   ## https://nodejs.org/api/path.html#path_path_join_paths
 
-func `/`*(path0: cstring; path1: cstring): cstring {.importjs: "path.join(#, #)".}
+func `/`*(path0: cstring; path1: cstring): cstring {.importjs: "(path.join(#, #) || '')".}
   ## Syntax sugar for `path.join()` that mimics Nim `path0 / path1`.
 
-func normalize*(path: cstring): cstring {.importjs: "path.$1(#)".}
+func normalize*(path: cstring): cstring {.importjs: "(path.$1(#) || '')".}
   ## https://nodejs.org/api/path.html#path_path_normalize_path
 
-func relative*(to, froom: cstring): cstring {.importjs: "path.$1(#, #)".}
+func relative*(to, froom: cstring): cstring {.importjs: "(path.$1(#, #) || '')".}
   ## https://nodejs.org/api/path.html#path_path_relative_from_to
 
-func resolve*(paths: cstring): cstring {.importjs: "path.$1(@)", varargs.}
+func resolve*(paths: cstring): cstring {.importjs: "(path.$1(@) || '')", varargs.}
   ## https://nodejs.org/api/path.html#path_path_resolve_paths
 
-func toNamespacedPath*(path: cstring): cstring {.importjs: "path.$1(#)".}
+func toNamespacedPath*(path: cstring): cstring {.importjs: "(path.$1(#) || '')".}
   ## https://nodejs.org/api/path.html#path_path_tonamespacedpath_path
 
-func parse*(path: cstring): JsObject {.importjs: "path.$1(#)".}
+func parse*(path: cstring): JsObject {.importjs: "(path.$1(#) || {})".}
   ## https://nodejs.org/api/path.html#path_path_parse_path
 
 when defined(windows):
