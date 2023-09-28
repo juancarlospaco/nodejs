f7e381eaa36ef7089b8a2e9b37db9b4054794624
Juan Carlos <juancarlospaco@gmail.com>
2023-09-28 11:49:55 -0300

    Delete src/nodejs/jsviewport.nim
    This is now part of stdlib.

--- src/nodejs/jsviewport.nim
+++
@@ -1,9 +0,0 @@
-## - https://developer.mozilla.org/en-US/docs/Web/API/VisualViewport
-import std/dom
-
-type VisualViewport* {.importc.} = ref object of EventTarget
-  offsetLeft*, offsetTop*, pageLeft*, pageTop*, width*, height*, scale*: float
-  onResize*: proc (event: Event) {.closure.}
-  onScroll*: proc (event: Event) {.closure.}
-
-func visualViewport*(self: Window): VisualViewport {.importjs: "#.$1", nodecl.}
